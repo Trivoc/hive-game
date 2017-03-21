3c6397b707af2a0de609217f94c94222524733d5
Trivoc <trivoc92@gmail.com>
2017-03-21 20:41:57 +0100

    added some TODOs

--- src/hive/model/Hive.java
+++ src/hive/model/Hive.java
@@ -15,6 +15,7 @@ public class Hive implements HexGraph {
 
     //TODO: Evaluate edge implementation!
     //Edges... put double links from each hexstack instead?
+    //Easier to remove from a shared edge than iterate all neighbours?
     private ArrayList<Edge> edges;
 
 
@@ -40,16 +41,16 @@ public class Hive implements HexGraph {
     //Adds edges to the hex specified at coordinates coords
     private void addEdges(AxialCoords coords){
         HexStack neighbour;
-
+        //TODO: Hard code neighbours instead? Probably easier to read and less lines either way.
         for(int x = -1; x <= 1; x++){
             for(int y = -1; y <= 1; y++){
                 if(x != y){
                     //debug (seems ok): System.out.println("Added: x+" + x + ", y+" + y );
                     neighbour = nodesMap.get(new AxialCoords
                             (coords.getX() + x, coords.getY() + y));
+                    //TODO: Add method for checking for neighbours or create new, empty stacks from them
                 }
             }
-
         }
     }
 
